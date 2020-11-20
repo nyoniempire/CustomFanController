5b7ae81b73640bbefd3a4bdd3390c394255f0347
chatunga <bruce@bloodhoundsa.com>
2020-11-20 15:59:44 +0200

    Finished working on the tutorial. Didn't add accessibility to the app but it's complete. Would work well with an IoT fan

--- app/src/main/java/com/ankh/customfancontroller/DialView.kt
+++ app/src/main/java/com/ankh/customfancontroller/DialView.kt
@@ -1,19 +1,29 @@
 package com.ankh.customfancontroller
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PointF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
 private enum class FanSpeed(val label: Int){
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
-    HIGH(R.string.fan_high)
+    HIGH(R.string.fan_high);
+
+    fun next() = when(this){
+        OFF->LOW
+        LOW->MEDIUM
+        MEDIUM-> HIGH
+        HIGH->OFF
+    }
 }
+
+
 
 private const val RADIUS_OFFSET_LABEL = 30
 private const val RADIUS_OFFSET_INDICATOR = -35
@@ -27,6 +37,10 @@ class DialView @JvmOverloads constructor(
     private var radius = 0.0f
     private var fanSpeed = FanSpeed.OFF
 
+    private var fanSpeedLowColor = 0
+    private var fanSpeedMediumColor =0
+    private var fanSpeedHighColor= 0
+
     private val pointPosition: PointF = PointF(0.0f,0.0f)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -36,13 +50,67 @@ class DialView @JvmOverloads constructor(
         typeface = Typeface.create("",Typeface.BOLD)
     }
 
+    init{
+        isClickable = true
+        context.withStyledAttributes(attrs,R.styleable.DialView){
+            fanSpeedLowColor = getColor(R.styleable.DialView_fanColor1,0)
+            fanSpeedMediumColor = getColor(R.styleable.DialView_fanColor2,0)
+            fanSpeedHighColor = getColor(R.styleable.DialView_fanColor3,0)
+        }
+
+        updateContentDescription()
+    }
+
+    override fun performClick(): Boolean {
+        if(super.performClick()) return true
+
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        updateContentDescription()
+
+        invalidate()
+        return true
+    }
+
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        radius = (min(w,h)/2.0 * 0.8).toFloat()
+    }
+
+    private fun PointF.computeXYForSpeed(pos: FanSpeed,radius: Float){
+        val startAngle = Math.PI*(9/8.0)
+        val angle = startAngle+pos.ordinal*(Math.PI/4)
+        x = (radius*cos(angle)).toFloat()+width/2
+        y = (radius* sin(angle)).toFloat()+height/2
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        paint.color = when(fanSpeed){
+            FanSpeed.OFF -> Color.GRAY
+            FanSpeed.LOW -> fanSpeedLowColor
+            FanSpeed.MEDIUM -> fanSpeedMediumColor
+            FanSpeed.HIGH -> fanSpeedHighColor
+        }
+
+        canvas!!.drawCircle((width/2).toFloat(),(height/2).toFloat(),radius,paint)
+
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+        pointPosition.computeXYForSpeed(fanSpeed,markerRadius)
+        paint.color = Color.BLACK
+        canvas.drawCircle(pointPosition.x,pointPosition.y,radius/12,paint)
+
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        for(i in FanSpeed.values()){
+            pointPosition.computeXYForSpeed(i,labelRadius)
+            val label = resources.getString(i.label)
+            canvas.drawText(label,pointPosition.x,pointPosition.y,paint)
+        }
     }
 
-
+    fun updateContentDescription(){
+        contentDescription = resources.getString(fanSpeed.label)
+    }
 }
+
